153bc9a960cf7a8dec7cbc57f5acc1fac27d0f68
Semianchuk Vitalii <fix20152@gmail.com>
2025-01-18 14:11:51 +0000

    Create task Set

---
+++ index.js
@@ -0,0 +1,30 @@
+class Set {
+    constructor() {
+       
+    }
+
+    // Add an element to the set
+    add(element) {
+       
+    }
+
+    // Remove an element from the set
+    delete(element) {
+      
+    }
+
+    // Check if an element exists in the set
+    has(element) {
+        
+    }
+
+    // Get the size of the set
+    size() {
+        
+    }
+
+    // Clear all elements from the set
+    clear() {
+       
+    }
+}
